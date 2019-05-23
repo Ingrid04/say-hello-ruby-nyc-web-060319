d8337af0a567f26b12648defa0de03dbf4986a33
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-23 04:10:03 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,9 +1,9 @@
 def say_hello (name) # Build your say_hello method here
   puts "Hello, #{name}!"
 end
-say_hello
+say_hello ("Ingrid")
 
 def say_hello (noName)
   puts "Hello, #{noName}!"
 end
-say_hello
+say_hello("Ruby Programmer")
