47ee4357ec298221d0a21f56c38e607fe48529c1
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-23 04:08:21 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,9 @@
 def say_hello (name) # Build your say_hello method here
-  puts "Hello, #{name}"
+  puts "Hello, #{name}!"
 end
+say_hello
 
 def say_hello (noName)
-  puts "Hello, #{noName}"
+  puts "Hello, #{noName}!"
 end
+say_hello
